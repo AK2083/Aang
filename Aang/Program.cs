e55413e974beb9429f427457dd85ecf859aa597a
Adrian Helmchen <adrian.helmchen@outlook.de>
2024-03-18 19:18:28 +0100

    correction

--- Aang/Program.cs
+++ Aang/Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration["ConnectionsStrings:DefaultConnection"]);
